d6e3601f94311e1f463c175a46573cdfc3992168
Atsushi Odagiri <aodagx@gmail.com>
2011-02-12 18:26:53 +0900

    easy_install

--- flexenv/__init__.py
+++ flexenv/__init__.py
@@ -2,45 +2,43 @@ import pkg_resources
 import site
 from argparse import ArgumentParser
 import os
+import tempfile
+import shutil
+import sys
 
-
-
-def list_versions(args, env):
-    project_name = args.project_name
-    dists = env[project_name]
-    for dist in dists:
-        print dist.version, dist.location
+def easy_install(project_name, dest):
+    results = []
+    tmp = tempfile.mkdtemp()
+    try:
+        args = os.path.join(os.environ['VIRTUAL_ENV'], 'bin', 'easy_install'), '-mUNxd', tmp, project_name
+        args += (dict(os.environ, PYTHONPATH=tmp),)
+        os.spawnle(os.P_WAIT, sys.executable, sys.executable, *args)
+        env = pkg_resources.Environment([tmp])
+        for d in env:
+            dist = env[d][0]
+            newloc = os.path.join(dest, os.path.basename(dist.location))
+            print 'move %s to %s' % (dist.location, newloc)
+            os.rename(dist.location, newloc)
+            results.append(pkg_resources.Environment([newloc])[dist.project_name][0])
+        return results
+    finally:
+        shutil.rmtree(tmp)
     
-def apply(args, env):
-    pth = os.path.join(args.virtualenv_dir, 'lib', 'python2.6', 'site-packages', 'flexenv.pth')
-    locations = []
-    project_name = args.project_name
-    dists = env[project_name]
-    for dist in dists:
-        locations.append(dist.location)
-        for req in dist.requires():
-            req_dists = env[req.project_name] 
-            for req_dist in req_dists:
-                locations.append(req_dist.location)
-    print "create %s" % pth
-    with open(pth, "wb") as f:
-        for loc in locations:
-            f.write(loc)
-            f.write('\n')
 
+    
 
 def develop(args, env):
     if 'VIRTUAL_ENV' not in os.environ:
         print >> os.stderr, "use virtualenv"
         sys.exit()
-
+    packages_dir = args.packages_dir
     pth = os.path.join(os.environ['VIRTUAL_ENV'], 'lib', 'python2.6', 'site-packages', 'flexenv.pth')
     develop_path = args.develop_path
     develop_env = pkg_resources.Environment(search_path=[develop_path])
     dists = []
     for x in develop_env:
         for dist in develop_env[x]:
-            dists.extend(_resolve_requires(dist, env))
+            dists.extend(_resolve_requires(dist, env, packages_dir))
 
     print 'create pth : %s ' % pth
     with open(pth, "wb") as f:
@@ -48,12 +46,17 @@ def develop(args, env):
             f.write(dist.location)
             f.write('\n')
 
-def _resolve_requires(dist, env):
+def _resolve_requires(dist, env, packagesdir):
     results = []
     for req in dist.requires():
         req_dists = env[req.project_name]
+        if len(req_dists) == 0:
+            print "%s needs %s" % (dist.project_name, req.project_name,)
+            results.extend(easy_install(req.project_name, packagesdir))
+            env.scan()
+            req_dists = env[req.project_name]
         for req_dist in req_dists:
-            results.extend(_resolve_requires(req_dist, env))
+            results.extend(_resolve_requires(req_dist, env, packagesdir))
             results.append(req_dist)
 
     return results
@@ -64,10 +67,6 @@ parser = ArgumentParser()
 parser.add_argument('--packages-dir', action="store", 
     default=site.USER_SITE)
 command_parser = parser.add_subparsers()
-list_versions_parser = command_parser.add_parser('list')
-list_versions_parser.add_argument('project_name', action="store")
-list_versions_parser.set_defaults(func=list_versions)
-
 develop_parser = command_parser.add_parser('develop')
 develop_parser.add_argument('develop_path')
 develop_parser.set_defaults(func=develop)
